dd62a97e49d33ebf4618231ef4a17c825536ca64
Filipe Silva <filipe.silva@opah.com.br>
2025-05-30 19:21:51 -0300

    refactor/configuração mapper

--- stock-control-backend/src/main/java/com/stock/stock_control_backend/config/StockMovementMapper.java
+++ stock-control-backend/src/main/java/com/stock/stock_control_backend/config/StockMovementMapper.java
@@ -5,19 +5,33 @@ import com.stock.stock_control_backend.model.StockMovement;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface StockMovementMapper {
 
-    @Mapping(target = "productId", source = "product.id")
-     StockMovementDTO toDto(StockMovement stockMovement);
+    default StockMovementDTO toDto(StockMovement movimento) {
+        StockMovementDTO dto = new StockMovementDTO();
+        dto.setId(movimento.getId());
+        dto.setProductId(movimento.getProduct().getId());
+        dto.setMovementType(movimento.getMovementType());
+        dto.setSalePrice(movimento.getSalePrice());
+        dto.setSaleDate(movimento.getSaleDate());
+        dto.setQuantityMovement(movimento.getQuantityMovement());
+        dto.setProductDescription(movimento.getProduct().getDescription());
+        if (movimento.getSaleDate() != null) {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+            dto.setSaleDateFormatted(movimento.getSaleDate().format(formatter));
+        }
+        return dto;
+    }
 
     @Mapping(target = "product.id", source = "productId")
-     StockMovement toEntity(StockMovementDTO stockMovementDTO);
+    StockMovement toEntity(StockMovementDTO stockMovementDTO);
 
-     List<StockMovementDTO> toDtoList(List<StockMovement> stockMovements);
+    List<StockMovementDTO> toDtoList(List<StockMovement> stockMovements);
 
-     List<StockMovement> toEntityList(List<StockMovementDTO> stockMovementDTOs);
+    List<StockMovement> toEntityList(List<StockMovementDTO> stockMovementDTOs);
 
 }
